820b8470c7f1bb9e283b1e97099575a11bc014fb
Ruko-1 <ojafolayan@gmail.com>
2025-06-21 15:28:53 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('aj-devs-cache').then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/style.css',
+        '/manifest.json',
+        '/images/omo3.png',
+        '/images/pwa-icon-192.png',
+        '/images/pwa-icon-512.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
